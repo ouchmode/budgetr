a2314b66756cb0df9b5568ae98372f284fb91642
ouchmode <anthony.rondini227@gmail.com>
2025-06-02 19:40:57 -0400

    some more code consolidation by moving this here from the main_menu method in ui.py

--- util/date_stuff.py
+++ util/date_stuff.py
@@ -20,3 +20,27 @@ def get_current_date_time_fmtd():
     """ used to default to the current date. """
     return datetime.today().strftime('%m/%d/%Y')
 
+
+def set_current_date_or_format_user_date():
+    """
+    if the user's date input is empty, use today's date. otherwise format their 
+    entered date.
+    """
+    while True:
+        new_date = input(f"Enter Transaction Date "
+                               f"(MM/DD/YYYY - can leave blank"
+                               f" for today's date): ").strip()
+        print("\n\n")
+
+        if new_date == "":
+            new_date = get_current_date_time_fmtd()
+            break
+        try:
+            parsed_date = datetime.strptime(new_date, 
+                                            "%m/%d/%Y")
+            new_date = parsed_date.strftime("%m/%d/%Y")
+            break
+        except ValueError:
+            print("Invalid date format. Please enter the date as MM/DD/YYYY.")
+
+    return new_date
