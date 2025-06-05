14086c8b7df769eee957b6c1e4d158a3835b00f4
ouchmode <anthony.rondini227@gmail.com>
2025-06-05 10:51:08 -0400

    updated

--- util/tables.py
+++ util/tables.py
@@ -2,11 +2,11 @@ from rich.table import Table
 from rich import box
 
 def all_txns_table_setup():
-    table = Table(title="//$---TRANSACTIONS---$//", 
-                  border_style="white", 
+    table = Table(border_style="white", 
                   box=box.SIMPLE_HEAD, 
                   row_styles=["dim", ""], 
-                  highlight=True)
+                  highlight=True,
+                  caption_justify="center")
 
     table.add_column("ID", width=10, header_style="magenta")
     table.add_column("AMOUNT", width=15, header_style="magenta")
