8be6126105c2df1101544e5cf99e376d032f7b78
ouchmode <anthony.rondini227@gmail.com>
2025-06-06 10:42:51 -0400

    added a console.clear() before the main_menu runs.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from rich.console import Console
+
 from classes.ui import UserInterface
 
 if __name__ == "__main__":
+    console = Console()
+    console.clear()
+
     ui = UserInterface()
     ui.main_menu()
