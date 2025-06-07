0663e2a3477b2580d73b7db9584e8d5425f25908
ouchmode <anthony.rondini227@gmail.com>
2025-06-07 09:57:33 -0400

    using os to clear the console instead of rich.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from rich.console import Console
-
 from classes.ui import UserInterface
 
 if __name__ == "__main__":
-    console = Console()
-    console.clear()
-
     ui = UserInterface()
+    # custom clear_console() method to clear the console using the os module.
+    # for some reason, some instances of powershell (at least on my machine) 
+    # don't clear the console properly with rich.
+    ui.clear_console()
     ui.main_menu()
